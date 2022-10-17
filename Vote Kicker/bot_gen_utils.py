7e7ad3ce9f629abc28c8ea51460bcc22e6b2a97d
IgalTam <igal.tamarkin@gmail.com>
2022-10-17 15:53:25 -0700

    modernization for Vote Kicker

---
+++ Vote Kicker/bot_gen_utils.py
@@ -0,0 +1,35 @@
+"""general use commands used by multiple bots"""
+
+import asyncio
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+def guild_find(ctx: commands.Context, bot, guild_name):
+    """returns guild object associated with guild_name, return None on fail"""
+    if guild_name is None:
+        return ctx.message.guild
+    else:
+        for guild_y in bot.guilds:
+            if guild_y.name == guild_name:
+                return guild_y
+        return None
+
+def user_find(ctx: commands.Context, guild: discord.Guild, usr):
+    """returns member object from guild"""
+    user = None
+    if usr is None:
+        user = ctx.author
+    else:
+        for member in guild.members:
+            if (member.name == usr or member.nick == usr) and member.id != guild.me.id:
+                user = member
+    return user
+
+def get_member_name(target: discord.Member):
+    """get member name output"""
+    if target is None:
+        return None
+    elif target.nick is None:
+        return target.name
+    return target.nick
--- Vote Kicker/vote_kicker_py.py
+++ Vote Kicker/vote_kicker_py.py
@@ -1,95 +1,99 @@
+from tkinter.tix import MAIN
+import bot_gen_utils
+from config import VOTEKICK
 import asyncio
 import discord
 from discord.ext import commands
-import aiohttp
 import warnings
 import datetime
 from dotenv import load_dotenv
 
 load_dotenv()
 
-DISCORD_TOKEN = 'OTI4ODU1MDYyMzYzNjM1ODAy.Yde1sA.400Tl6v92AUpxEC2Gl50kheduSk'
+DISCORD_TOKEN = VOTEKICK
+OP_ID = 386648374469722144 # put your discord ID here if you want
+                           # maintenance enabling
+MAINT_FLAG = False # prevents operator from getting votekicked while working on bot
 
-warnings.filterwarnings("ignore", category=DeprecationWarning)
-intents = discord.Intents.all()
-bot = commands.Bot(command_prefix='!*!', intents=intents)
-bot.session = aiohttp.ClientSession()
+class Bot(commands.Bot):
+    def __init__(self):
+        intents = discord.Intents.all()
+        intents.message_content = True
+        warnings.filterwarnings("ignore", category=DeprecationWarning)
+        super().__init__(command_prefix="!*!", intents=intents,\
+             status=discord.Status.offline)
+    
+    async def setup_hook(self) -> None:
+        await self.tree.sync()
+        print(f"Synced slash commands for {self.user}.")
 
+    async def on_command_error(self, ctx, error):
+        await ctx.reply(error, ephemeral=True)
 
-async def timeout_user(*, user_id: int, guild_id: int, until):
-    headers = {"Authorization": f"Bot {bot.http.token}"}
-    url = f"https://discord.com/api/v9/guilds/{guild_id}/members/{user_id}"
-    timeout = (datetime.datetime.utcnow() + datetime.timedelta(minutes=until)).isoformat()
-    json = {'communication_disabled_until': timeout}
-    print(json)
-    async with bot.session.patch(url, json=json, headers=headers) as session:
-        print(session.status)
-        if session.status in range(200, 299):
-            return True
-        return False
+bot = Bot()
 
-
-@bot.command(name='votetimeout', help='votetimeout [name] [duration (minutes)]: hold vote on whether to '
-                                      'timeout someone, and for how long (DOES NOT WORK ON ADMINS)')
-async def votetimeout(ctx, kicked, durat, timer=30.0):
-    maint_flag = False
-    kicked_obj = get_member(kicked, ctx.message.guild)
-    if kicked_obj is None:
-        return await ctx.send(f"{kicked} is not a member of this server.")
-    if ctx.message.author.id == 168015898148470784 and kicked.id == 145396599621812226:
-        await ctx.send(f"Stop it Pavel that's mean")
-    elif ctx.message.author.id == 168015898148470784 and kicked_obj.id == 386648374469722144 and maint_flag is True:
-        await ctx.send(f"{kicked} is attempting to fix me, stop interrupting")
+@bot.hybrid_command(name="votetimeout", with_app_command=True, description="vote on whether to timeout"\
+    "someone and for how long")
+@commands.has_permissions(moderate_members=True)
+async def votetimeout(ctx: commands.Context, kicked: discord.Member, timeout_duration, timer=30.0):
+    MAINT_FLAG = False
+    kicked_name = bot_gen_utils.get_member_name(kicked)
+    if kicked is None:
+        return await ctx.send(f"{kicked_name} is not a member of this server.")
+    if kicked.id == OP_ID and MAINT_FLAG is True:
+        await ctx.send(f"{kicked_name} is attempting to fix me, stop interrupting")
     else:
+        await ctx.send(f"Timeout for {kicked_name} is now pending.")
         result = await vote(ctx, timer)
-        if result:
-            timeout_check = await timeout_user(user_id=kicked_obj.id, guild_id=ctx.message.guild.id, until=int(durat))
-            if timeout_check:
-                return await ctx.send(f"Vote result passed. Timeout for {durat} minutes.")
-            await ctx.send("Something went wrong. Vote result terminated.")
+        if result == 1:
+            try:
+                await kicked.timeout(datetime.timedelta(int(timeout_duration)), reason="The people voted for it.")
+                await ctx.send(f"Vote result passed. {kicked_name} is timed out for {timeout_duration} minutes.")
+            except:
+                await ctx.send("Something went wrong. Vote result terminated.")
+        elif result == 0:
+            await ctx.send("Vote result is tied. No action taken.")
         else:
             await ctx.send("Vote result not passed.")
 
 
-@bot.command(name='votekick', help='votekick [name]: hold vote on whether to disconnect someone from vc')
-async def votekick(ctx, kicked, timer=30.0):
-    maint_flag = False
-    kicked_obj = get_member(kicked, ctx.message.guild)
-    if kicked_obj is None:
-        return await ctx.send(f"{kicked} is not a member of this server.")
-    elif kicked_obj.id == 386648374469722144 and maint_flag is True:
-        await ctx.send(f"{kicked} is attempting to fix me, stop interrupting")
+@bot.hybrid_command(name='votekick', with_app_command=True, description='vote on whether to '\
+    'kick a user from the voice channel they are connected to')
+async def votekick(ctx: commands.Context, kicked: discord.Member, timer=30.0):
+    kicked_name = bot_gen_utils.get_member_name(kicked)
+    if kicked_name is None:
+        return await ctx.send(f"{kicked_name} is not a member of this server.")
+    elif kicked.id == OP_ID and MAINT_FLAG is True:
+        await ctx.send(f"{kicked_name} is attempting to fix me, stop interrupting")
+    elif kicked.voice is None:
+        print("no voice")
+        await ctx.send(f"{kicked_name} is not connected to a voice channel.")
     else:
+        await ctx.send(f"VC kick for {kicked_name} is now pending.")
         result = await vote(ctx, timer)
-        if result:
-            await kicked_obj.move_to(None)
-            await ctx.send(f"Vote result passed. {kicked} kicked from voice channel.")
+        if result == 1:
+            await kicked.move_to(None)
+            await ctx.send(f"Vote result passed. {kicked_name} kicked from voice channel.")
+        elif result == 0:
+            await ctx.send("Vote result is tied. No action taken.")
         else:
             await ctx.send("Vote result not passed.")
 
 
-def get_member(name, guild):
-    """discord.py got changed, need this to find member"""
-    print(guild.members)
-    for member in guild.members:
-        if member.name == name or member.nick == name:
-            return member
-    return None
-
-
-@bot.command(name='genvote', help='genvote [message]: vote yes or no to message (put message in "" if '
-                                  'it contains spaces)')
-async def genvote(ctx, message, timer=30):
+@bot.hybrid_command(name='genvote', with_app_command=True, description="create a Y/N vote for input message")
+async def genvote(ctx: commands.Context, message, timer=30):
     await ctx.send(message)
     result = await vote(ctx, timer)
-    if result:
+    if result == 1:
         await ctx.send("Yes")
+    elif result == 0:
+        await ctx.send("Tie")
     else:
         await ctx.send("No")
 
 
-@bot.command()
 async def vote(ctx, durat):
+    """voting-enabling function, uses emojis to represent option weights"""
     message = await ctx.send("React to this message to vote.")
     await asyncio.gather(message.add_reaction('👍'), message.add_reaction('👎'))
 
@@ -99,16 +103,20 @@ async def vote(ctx, durat):
     try:
         await bot.wait_for('message', timeout=durat, check=check)
         cache_msg = discord.utils.get(bot.cached_messages, id=message.id)
-        if cache_msg.reactions[0].count > cache_msg.reactions[1].count:
-            return True
+        if cache_msg.reactions[0].count == cache_msg.reactions[1].count:
+            return 0 # tie
+        elif cache_msg.reactions[0].count > cache_msg.reactions[1].count:
+            return 1 # yes
         else:
-            return False
+            return 2 # no
     except asyncio.TimeoutError:
         cache_msg = discord.utils.get(bot.cached_messages, id=message.id)
-        if cache_msg.reactions[0].count > cache_msg.reactions[1].count:
-            return True
+        if cache_msg.reactions[0].count == cache_msg.reactions[1].count:
+            return 0 # tie
+        elif cache_msg.reactions[0].count > cache_msg.reactions[1].count:
+            return 1 # yes
         else:
-            return False
+            return 2 # no
 
 
 @bot.event
@@ -116,13 +124,7 @@ async def on_ready():
     print("bot ready")
 
 
-"""@bot.event
-async def on_reaction_add(reaction, user):
-    print("someone reacted")
-    print(reaction.message.reactions)"""
-
-
-@bot.command(name='ping', help='pingpong')
+@bot.hybrid_command(name='ping', with_app_command=True, description='pingpong')
 async def ping(ctx):
     await ctx.send('Pong! {0}'.format(round(bot.latency, 1)))
 
