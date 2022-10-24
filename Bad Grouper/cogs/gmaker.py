7834646325e5ebb4f7d73427a2650410e94d05bf
IgalTam <igal.tamarkin@gmail.com>
2022-10-23 23:56:28 -0700

    enabled gmaker compatibility with non-ascii characters

--- Bad Grouper/cogs/gmaker.py
+++ Bad Grouper/cogs/gmaker.py
@@ -7,7 +7,7 @@ import tempfile
 
 class Gmaker(commands.Cog):
     """commands for creating guild channels from input files
-    and creating input files via interactive input"""
+    and creating input files via interactive input or guild captures"""
 
     def __init__(self, bot: commands.Bot):
         self.bot = bot
@@ -22,7 +22,7 @@ class Gmaker(commands.Cog):
         return commands.check(predicate)
     
     @commands.hybrid_command(name="gmake", with_app_command=True,\
-        description="creates channels based on an input file")
+        description="creates channels based on a \"gmake\" input file")
     async def gmake(self, ctx: commands.Context, infile: discord.Attachment):
         
         # parse file
@@ -35,9 +35,8 @@ class Gmaker(commands.Cog):
         await ctx.send("Processing file...")
         try:
             await infile.save(fp=infile.filename) # save attachment as file-like obj for reading
-            with open(infile.filename, mode='r') as fo:
-                fconts = fo.read()
-                chnls = fconts.split('\n')
+            with open(infile.filename, mode='rb') as fo: # open file as binary in case of non-ascii characters
+                chnls = fo.read().decode('utf-8').split('\n')
             fo.close()
             cur_cat = None
             succ_chns = 0
@@ -48,25 +47,34 @@ class Gmaker(commands.Cog):
                     chnl_arr = chnl.split(':')
                     chnl_type, chnl_nm = chnl_arr[0], chnl_arr[1]
                     if chnl_type == "cat":
-                        cur_cat = await ctx.message.guild.create_category(chnl_nm)
+                        try:
+                            cur_cat = await ctx.message.guild.create_category(chnl_nm.decode('utf-8'))
+                        except AttributeError:
+                            cur_cat = await ctx.message.guild.create_category(chnl_nm)
                         succ_chns += 1
                     elif chnl_type == "tc":
-                        await ctx.message.guild.create_text_channel(chnl_nm, category=cur_cat)
+                        try:
+                            await ctx.message.guild.create_text_channel(chnl_nm.decode('utf-8'), category=cur_cat)
+                        except AttributeError:
+                            await ctx.message.guild.create_text_channel(chnl_nm, category=cur_cat)
                         succ_chns += 1
                     elif chnl_type == "vc":
-                        await ctx.message.guild.create_voice_channel(chnl_nm, category=cur_cat)
+                        try:
+                            await ctx.message.guild.create_voice_channel(chnl_nm.decode('utf-8'), category=cur_cat)
+                        except AttributeError:
+                            await ctx.message.guild.create_voice_channel(chnl_nm, category=cur_cat)
                         succ_chns += 1
                     else:
                         await ctx.send("Skipping invalid channel...")
             os.unlink(fo.name)
-            await ctx.send(f"Operation complete. {len(succ_chns)} channels created.")
+            await ctx.send(f"Operation complete. {succ_chns} channels created.")
         except FileNotFoundError:
             await ctx.send("File not found. Make sure to include the complete path.")
         
 
     @commands.hybrid_command(name="cgmake", with_app_command=True,\
         description="clears all channels in guild, then creates new"\
-        " channels from input file")
+        " channels from a \"gmake\" input file")
     @guild_owner_only()
     async def cgmake(self, ctx: commands.Context, infile: discord.Attachment):
         # confirm owner intentions
@@ -99,7 +107,8 @@ class Gmaker(commands.Cog):
             await ctx.message.channel.delete()
     
     @commands.hybrid_command(name="gmakesnap", with_app_command=True,\
-        description="saves current channel layout as \"gmake\" text file")
+        description="saves current channel hierarchy as a \"gmake\" text file")
+    @commands.has_permissions(administrator=True)
     async def gmakesnap(self, ctx: commands.Context, des_filename: str):
         # parse filename
         await ctx.send("Parsing filename...")
@@ -109,20 +118,20 @@ class Gmaker(commands.Cog):
 
         # write channels to file
         await ctx.send("Creating \"gmake\" file...")
-        with open(f"{des_filename}.txt", 'w') as fp:
+        with open(f"{des_filename}.txt", 'wb') as fp: # open file as binary in case of non-ascii characters
             for chnl_tup in ctx.message.guild.by_category():
                 # get category channel
                 if chnl_tup[0] is not None:
-                    fp.write(f"cat:{chnl_tup[0].name}\n")
+                    fp.write(f"cat:{chnl_tup[0].name}\n".encode('utf-8'))
                 # get category member channels
                 for sub_chnl in chnl_tup[1]:
                     if isinstance(sub_chnl, discord.CategoryChannel):
-                        fp.write("cat:")
+                        fp.write("cat:".encode('UTF-8'))
                     elif isinstance(sub_chnl, discord.TextChannel):
-                        fp.write("tc:")
+                        fp.write("tc:".encode('utf-8'))
                     else:
-                        fp.write("vc:")
-                    fp.write(f"{sub_chnl.name}\n")
+                        fp.write("vc:".encode('utf-8'))
+                    fp.write(f"{sub_chnl.name}\n".encode('utf-8'))
         
         # send file to user and clean up
         fp.close()
@@ -158,12 +167,12 @@ class Gmaker(commands.Cog):
             await ctx.send("New channel. What type would you like it to be? [text/voice]")
             msg = await self.bot.wait_for("message", check=check_chnl_type)
             if msg.content.lower() == "t" or msg.content.lower() == "tc" or msg.content.lower() == "text" or msg.content.lower() == "text channel":
-                file_pointer.write("tc:")
+                file_pointer.write("tc:".encode('utf-8'))
             elif msg.content.lower() == "v" or msg.content.lower() == "vc" or msg.content.lower() == "voice" or msg.content.lower() == "voice channel":
-                file_pointer.write("vc:")
+                file_pointer.write("vc:".encode('utf-8'))
             await ctx.send("What name would you like this channel to have?")
             msg = await self.bot.wait_for("message", check=check_chnl_cont)
-            file_pointer.write(f"{msg.content}\n")
+            file_pointer.write(f"{msg.content}\n".encode('utf-8'))
 
             # check if user wants to create more channels in this category
             await ctx.send("Would you like to include another channel in this category? [Y/N]")
@@ -173,7 +182,7 @@ class Gmaker(commands.Cog):
             return False
 
         # create new file
-        fp = open(f"{des_filename}.txt", 'w')
+        fp = open(f"{des_filename}.txt", 'wb') # open file as binary in case of non-ascii characters
         
         # interactive ops
         try:
@@ -199,10 +208,10 @@ class Gmaker(commands.Cog):
                 await ctx.send("Do you wish to include a new category? [Y/N]")
                 msg = await self.bot.wait_for("message", check=check, timeout=60)
                 if msg.content.lower() == "y" or msg.content.lower() == "yes":
-                    fp.write("cat:")
+                    fp.write("cat:".encode('utf-8'))
                     await ctx.send("What name would you like this category to have?")
                     msg = await self.bot.wait_for("message", check=check_chnl_cont)
-                    fp.write(f"{msg.content}\n")
+                    fp.write(f"{msg.content}\n".encode('utf-8'))
 
                     # create category member channels
                     await ctx.send("Do you wish to include new channels in this category? [Y/N]")
